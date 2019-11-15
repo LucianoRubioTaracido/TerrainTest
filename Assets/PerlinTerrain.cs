f7c4ab7ba7416d204dfe04e3d5a67c78c3ab0cde
LucianoRubioTaracido <53436563+LucianoRubioTaracido@users.noreply.github.com>
2019-11-15 11:42:26 -0600

    Project Setup

---
+++ Assets/PerlinTerrain.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+public class PerlinTerrain : MonoBehaviour
+{
+    public int depth = 20;
+    private int width =  256;
+    private int height = 256;
+    public float scale =  20;
+
+    public float offsetX = 100f;
+    public float offsetY = 100f;
+
+    private void Start()
+    {
+        offsetX = Random.Range(0f, 9999f);
+        offsetY = Random.Range(0f, 9999f);
+    }
+
+    private void Update()
+    {
+        Terrain terrain = GetComponent<Terrain>();
+        terrain.terrainData = GenTerrain(terrain.terrainData);
+
+        offsetX += Time.deltaTime * .2f;
+    }
+
+    TerrainData GenTerrain (TerrainData terrainData)
+    {
+        terrainData.heightmapResolution = width + 1;
+        terrainData.size = new Vector3(width, depth, height);
+        terrainData.SetHeights(0, 0, GenHeights());
+        return terrainData;
+    }
+
+    float[,] GenHeights()
+    {
+        float[,] heights = new float[width, height];
+        for (uint x = 0; x < width; x++)
+        {
+            for (uint y = 0; y < height; y++)
+            {
+                heights[x, y] = CalculateHeight((int)x, (int)y);
+            }
+        }
+        return heights;
+    }
+
+    float CalculateHeight(int x, int y)
+    {
+        float xCoord = (float)x / width  * scale + offsetX;
+        float yCoord = (float)y / height * scale + offsetY;
+        return Mathf.PerlinNoise(xCoord, yCoord);
+    }
+
+}
